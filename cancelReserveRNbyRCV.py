5bedbf351cdf1108e91ffff1ed7a07f264d7069e
redbean88 <doctortor88@gmail.com>
2022-10-28 11:40:27 +0900

    sms 일부취소 추가

---
+++ cancelReserveRNbyRCV.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import MessageService, PopbillException
+
+messageService = MessageService(testValue.LinkID, testValue.SecretKey)
+messageService.IsTest = testValue.IsTest
+messageService.IPRestrictOnOff = testValue.IPRestrictOnOff
+messageService.UseStaticIP = testValue.UseStaticIP
+messageService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+파트너가 할당한 전송요청 번호와 수신번호를 통해 예약접수된 문자 전송을 취소합니다. (예약시간 10분 전까지 가능)
+- https://docs.popbill.com/message/python/api#CancelReserveRNbyRCV
+'''
+
+try:
+    print("=" * 15 + " 예약문자메시지 전송취소 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 예약문자전송시 할당한 전송요청번호(requestNum)
+    requestNum = ""
+
+    # 예약문자전송시 할당한 수신번호
+    receiveNum = ""
+
+    result = messageService.cancelReserveRNbyRCV(CorpNum, requestNum, receiveNum)
+
+    print("처리결과 : [%d] %s" % (result.code, result.message))
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
---
+++ cancelReservebyRCV.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import MessageService, PopbillException
+
+messageService = MessageService(testValue.LinkID, testValue.SecretKey)
+messageService.IsTest = testValue.IsTest
+messageService.IPRestrictOnOff = testValue.IPRestrictOnOff
+messageService.UseStaticIP = testValue.UseStaticIP
+messageService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+팝빌에서 반환받은 접수번호와 수신번호를 통해 예약접수된 문자 메시지 전송을 취소합니다. (예약시간 10분 전까지 가능)
+- https://docs.popbill.com/message/python/api#CancelReservebyRCV
+'''
+
+try:
+    print("=" * 15 + " 예약문자메시지 전송취소 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 예약문자전송시 발급받은 접수번호(receiptNum)
+    receiptNum = ""
+
+    # 예약문자전송시 할당한 수신번호
+    receiveNum = ""
+
+    result = messageService.cancelReservebyRCV(CorpNum, receiptNum, receiveNum)
+
+    print("처리결과 : [%d] %s" % (result.code, result.message))
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
