454335d401242fb15491739256b1486d2d41a698
AlBan93 <albaneo0724@gmail.com>
2023-03-17 16:52:13 +0900

    파라미터 주석 추가, import 정렬 및 코드 포맷

--- paymentRequest.py
+++ paymentRequest.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # code for console Encoding difference. Dont' mind on it
-import sys
 import imp
+import sys
 
 imp.reload(sys)
 try:
@@ -10,8 +10,7 @@ except Exception as E:
     pass
 
 import testValue
-
-from popbill import MessageService, PopbillException, PaymentForm
+from popbill import MessageService, PaymentForm, PopbillException
 
 messageService = MessageService(testValue.LinkID, testValue.SecretKey)
 messageService.IsTest = testValue.IsTest
