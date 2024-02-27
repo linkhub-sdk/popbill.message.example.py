b5b37ee4db595aa2916619605d8eef63498243e7
정우석 <wsj@linkhubcorp.com>
2024-02-27 09:50:34 +0900

    주석(용어) 수정

--- testValue.py
+++ testValue.py
@@ -3,15 +3,21 @@
 """
 팝빌 문자 API Python SDK Example
 
-- Python SDK 연동환경 설정방법 안내 : https://developers.popbill.com/guide/sms/python/getting-started/tutorial
-- 업데이트 일자 : 2023-05-10
-- 연동 기술지원 연락처 : 1600-9854
-- 연동 기술지원 이메일 : code@linkhubcorp.com
+Python 연동 튜토리얼 안내 : https://developers.popbill.com/guide/sms/python/getting-started/tutorial
+업데이트 일자 : 2024-02-27
+연동 기술지원 연락처 : 1600-9854
+연동 기술지원 이메일 : code@linkhubcorp.com
 
 <테스트 연동개발 준비사항>
-1) 20, 23번 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
-   연동신청 후 메일로 발급받은 인증정보를 참조하여 변경합니다.
-2) 발신번호 사전등록을 합니다. (등록방법은 사이트/API 두가지 방식이 있습니다.)
+1) API Key 변경 (연동신청 시 메일로 전달된 정보)
+   - LinkID : 링크허브에서 발급한 링크아이디
+   - SecretKey : 링크허브에서 발급한 비밀키
+2) SDK 환경설정 옵션 설정
+   - IsTest : 연동환경 설정, True-테스트, False-운영(Production), (기본값:True)
+   - IPRestrictOnOff : 인증토큰 IP 검증 설정, True-사용, False-미사용, (기본값:True)
+   - UseStaticIP : 통신 IP 고정, True-사용, False-미사용, (기본값:False)
+   - UseLocalTimeYN : 로컬시스템 시간 사용여부, True-사용, False-미사용, (기본값:True)
+1) 발신번호 사전등록을 합니다. (등록방법은 사이트/API 두가지 방식이 있습니다.)
    - 1. 팝빌 사이트 로그인 > [문자/팩스] > [문자] > [발신번호 사전등록] 메뉴에서 등록
    - 2. getSenderNumberMgtURL API를 통해 반환된 URL을 이용하여 발신번호 등록
 """
@@ -22,20 +28,20 @@ LinkID = "TESTER"
 # 비밀키
 SecretKey = "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I="
 
-# 연동환경 설정값, 개발용(True), 상업용(False)
+# 연동환경 설정, True-테스트, False-운영(Production), (기본값:True)
 IsTest = True
+
+# 인증토큰 IP 검증 설정, True-사용, False-미사용, (기본값:True)
+IPRestrictOnOff = True
+
+# 통신 IP 고정, True-사용, False-미사용, (기본값:False)
+UseStaticIP = False
+
+# 로컬시스템 시간 사용여부, True-사용, False-미사용, (기본값:True)
+UseLocalTimeYN = True
 
 # 팝빌회원 사업자번호
 testCorpNum = "1234567890"
 
 # 팝빌회원 팝빌 아아디
 testUserID = "testkorea"
-
-# 발급토큰 IP 제한기능 활성화 여부 (권장-True)
-IPRestrictOnOff = True
-
-# 팝빌 API 서비스 고정 IP 사용여부, true-사용, false-미사용, 기본값(false)
-UseStaticIP = False
-
-# 로컬시스템 시간 사용여부, 권장(True)
-UseLocalTimeYN = True
